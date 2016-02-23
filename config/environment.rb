b0c2ec6d84ce55be42c4a44a8f2a122f356ef1dc
huaxiukun <huaxiukun@yiyc.com>
2016-02-23 22:19:41 +0800

    initi

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-require 'carrierwave'
-require 'carrierwave/orm/activerecord'
+
