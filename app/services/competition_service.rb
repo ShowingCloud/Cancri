30152dfcb0bfa8ae10d7c57e68ce1f80fb8e0623
huaxiukun <huaxiukun@yiyc.com>
2016-03-21 12:28:08 +0800

    test

--- app/services/competition_service.rb
+++ app/services/competition_service.rb
@@ -3,6 +3,12 @@ class CompetitionService
     Competition.includes(:competition_schedules).map { |x| {
         id: x.id,
         name: x.name,
+        status: x.status,
+        guide_units: x.guide_units,
+        organizer_units: x.organizer_units,
+        help_units: x.help_units,
+        support_units: x.support_units,
+        undertake_units: x.undertake_units,
         schedule: x.competition_schedules.select(:name, :start_time, :end_time).order('start_time asc').map { |s| {
             step: s.name,
             start_time: s.start_time,
