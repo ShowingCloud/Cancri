df7cce28458857c8c97fc6c821ec6656aa99c3c8
huaxiukun <huaxiukun@yiyc.com>
2016-03-25 14:33:46 +0800

    cors

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,14 @@ module Dome
   class Application < Rails::Application
 
 
-    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
-    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
     config.autoload_paths.push(*%W(#{config.root}/lib))
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
 
     config.time_zone = 'Asia/Shanghai'
     config.active_record.default_timezone = :local
