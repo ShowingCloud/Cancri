b0b851a8473267d10ac772d6b18a230a643d421d
huaxiukun <huaxiukun@yiyc.com>
2016-04-14 12:44:26 +0800

    register

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,10 +12,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     # super
     build_resource(sign_up_params)
     resource.nickname = params[resource_name][:nickname]
-
-    yield resource if block_given?
-
     if verify_rucaptcha?(resource)
+      resource.save
+      yield resource if block_given?
       if resource.persisted?
         if resource.active_for_authentication?
           set_flash_message :notice, :signed_up if is_flashing_format?
@@ -34,22 +33,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     else
       render action: 'new'
     end
-
-    # if resource.persisted?
-    #   if resource.active_for_authentication?
-    #     set_flash_message :notice, :signed_up if is_flashing_format?
-    #     sign_up(resource_name, resource)
-    #     respond_with resource, location: after_sign_up_path_for(resource)
-    #   else
-    #     set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-    #     expire_data_after_sign_in!
-    #     respond_with resource, location: after_inactive_sign_up_path_for(resource)
-    #   end
-    # else
-    #   clean_up_passwords resource
-    #   set_minimum_password_length
-    #   respond_with resource
-    # end
   end
 
 # GET /resource/edit
