c4bffa7366ba9472088f44ebd455298b934fdc66
huaxiukun <huaxiukun@yiyc.com>
2016-04-18 10:11:57 +0800

    cud

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,5 +1,5 @@
 class CompetitionsController < ApplicationController
-  # before_action :require_user, except: [:index, :show, :invite]
+  before_action :require_user, except: [:index, :show, :invite]
   before_action :set_competition, only: [:show]
   layout 'invite', only: [:invite]
 
@@ -26,7 +26,7 @@ class CompetitionsController < ApplicationController
 
   def already_apply
     if params[:ed].present?
-      a_p = TeamUserShip.where(event_id: params[:ed], user_id: 1).take
+      a_p = TeamUserShip.where(event_id: params[:ed], user_id: current_user.id).take
       if a_p.present?
         team_players = Team.find_by_sql("select a.id,u_p.username,u_p.grade as grade,u_p.user_id as user_id,u_p.gender as gender, a.status,t.name as name,s.name as school from team_user_ships a INNER JOIN teams t on t.id = a.team_id inner join user_profiles u_p on u_p.user_id = a.user_id inner join schools s on s.id = u_p.school where a.team_id = #{a_p.team_id}")
         result =[true, team_players, a_p.status]
