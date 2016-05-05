07b7127b774e9f781636e90424cfb333848bc829
huaxiukun <huaxiukun@yiyc.com>
2016-05-05 10:49:44 +0800

    teacher info

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -30,7 +30,7 @@ class CompetitionsController < ApplicationController
       if event.present?
         a_p = TeamUserShip.where(event_id: params[:ed], user_id: current_user.id).take
         if a_p.present?
-          team_players = Team.find_by_sql("select t.user_id as id,a.team_id,u_p.username,u_p.bj,t.identifier,u_p.grade as grade,u_p.user_id as user_id,u_p.gender as gender, a.status,t.name as name,s.name as school from team_user_ships a INNER JOIN teams t on t.id = a.team_id inner join user_profiles u_p on u_p.user_id = a.user_id inner join schools s on s.id = u_p.school where a.team_id = #{a_p.team_id}")
+          team_players = Team.find_by_sql("select t.user_id as id,a.team_id,u_p.username,u_p.bj,t.identifier,t.teacher,t.teacher_mobile,u_p.grade as grade,u_p.user_id as user_id,u_p.gender as gender, a.status,t.name as name,s.name as school from team_user_ships a INNER JOIN teams t on t.id = a.team_id inner join user_profiles u_p on u_p.user_id = a.user_id inner join schools s on s.id = u_p.school where a.team_id = #{a_p.team_id}")
           result =[true, team_players, event.group]
         else
           result = [false, '未报名', event.group]
