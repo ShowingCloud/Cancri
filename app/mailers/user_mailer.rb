d64d01c5fd7dba9c7b54548eac4049ba0bae5e5a
huaxiukun <huaxiukun@yiyc.com>
2016-05-24 17:16:32 +0800

    team_user status

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -17,6 +17,6 @@ class UserMailer < ActionMailer::Base
     @event = event
     @email = invited_email
     email_with_name = "<#{invited_email}>"
-    mail(to: email_with_name, subject: "#{leader}邀请你参加#{event}比赛")
+    mail(to: email_with_name, subject: "#{leader}邀请你参加#{event}项目")
   end
 end
