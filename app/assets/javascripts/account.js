fe0a7947c52acdfcb5a49f66ba0033be1a30e1bf
lmiogit <yl_mio@sina.com>
2016-06-17 13:53:51 +0800

    test

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -10,9 +10,10 @@ $(function () {
         if (is_sending) {
             return;
         }
-        mobile = $('#' + self.attr('data-key') + '_mobile');
+        mobile = $('#' + self.attr('data-key'));
         captcha = $('input[name="_rucaptcha"]').val();
         mobile_number = mobile.val();
+        console.log(mobile_number);
         if (captcha == '' || captcha == null) {
             alert('请输入校验码');
             $('input[name="_rucaptcha"]').focus();
