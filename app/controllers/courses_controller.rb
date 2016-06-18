f9c65b55188c975b14f19371f5308670fd9de581
huaxiukun <huaxiukun@yiyc.com>
2016-06-18 13:00:01 +0800

    course show profile first

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -19,7 +19,7 @@ class CoursesController < ApplicationController
     end
     @course = course
     if current_user.present?
-      @user_info = UserProfile.left_joins(:school, :district).where(user_id: current_user.id).select(:grade, :username, :district_id, :school_id, 'districts.name as district_name', 'schools.name as school_name').first ||= current_user.build_user_profile
+      @user_info = (UserProfile.left_joins(:school, :district).where(user_id: current_user.id).select(:grade, :username, :district_id, :school_id, 'districts.name as district_name', 'schools.name as school_name').first) ||= current_user.build_user_profile
     end
   end
 
