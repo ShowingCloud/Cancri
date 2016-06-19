e3f2050d5ed2276897a0a95c354f8865c6d859dd
WANG Guoqin <wangguoqin1001@gmail.com>
2016-06-19 20:21:58 +0800

    Remove old migrations

--- db/migrate/20160328093601_add_columns_to_user_profiles.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToUserProfiles < ActiveRecord::Migration[5.0]
-  def change
-    add_column :user_profiles, :roles, :string
-    add_column :user_profiles, :sk_station, :integer
-    add_column :user_profiles, :standby_school, :integer
-    add_index :user_profiles, :roles
-    add_index :user_profiles, :sk_station
-  end
-end
--- db/migrate/20160419074228_add_columns_to_scores.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToScores < ActiveRecord::Migration[5.0]
-  def change
-    add_column :scores, :device_no, :string
-    add_index :scores, :device_no
-  end
-end
