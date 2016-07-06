90e7a4cc0cfe8403bcf4c3a5ef3518b1346884dd
huaxiukun <huaxiukun@yiyc.com>
2016-07-06 15:01:11 +0800

    school_type allow null

--- db/migrate/20160706065657_change_columns_to_schools.rb
+++ db/migrate/20160706065657_change_columns_to_schools.rb
@@ -1,6 +1,6 @@
 class ChangeColumnsToSchools < ActiveRecord::Migration[5.0]
   def change
-    change_column :schools, :school_type, :integer
+    change_column :schools, :school_type, :integer, null: true
     remove_index :schools, [:district_id, :school_type, :name]
     remove_index :schools, :name
     add_index :schools, :name, unique: true
