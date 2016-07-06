4d7f1caa337df944865157f975bf7c9529a2d964
huaxiukun <huaxiukun@yiyc.com>
2016-07-06 16:22:15 +0800

    application js

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,6 +1,7 @@
 class School < ApplicationRecord
   has_many :user_profiles
   has_many :course_user_ships
+  belongs_to :district
   validates :name, presence: true
   validates :district_id, presence: true
   # validates :school_type, presence: true
