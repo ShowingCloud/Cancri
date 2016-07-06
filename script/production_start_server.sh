942a38b6d989b8dbf7336b5ee263eb9c6dafc058
WANG Guoqin <wangguoqin1001@gmail.com>
2016-07-07 03:48:55 +0800

    New production_start_server.sh

--- script/production_start_server.sh
+++ script/production_start_server.sh
@@ -2,5 +2,7 @@
 
 set -x
 
+sidekiqctl stop tmp/pids/sidekiq.pid 60
 thin -p 3240 -e production stop
 thin -d -p 3240 -e production --tag "Robodou Production" start
+sidekiq -C config/sidekiq.yml -d -e production --tag "Robodou Production"
