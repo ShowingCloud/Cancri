8a4f1cd3a0311ef49738db2376ee445d872a311b
huaxiukun <huaxiukun@yiyc.com>
2016-08-03 10:10:18 +0800

    news require cover

--- app/models/news.rb
+++ app/models/news.rb
@@ -4,5 +4,7 @@ class News < ApplicationRecord
   validates :news_type, presence: true
   validates :content, presence: true
   validates :admin_id, presence: true
+  validates :desc, presence: true
+  validates :cover, presence: true
   mount_uploader :cover, CoverUploader
 end
