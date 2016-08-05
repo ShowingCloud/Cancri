b4116f4cc43c666e405437a79f50563732f4e6ac
huaxiukun <huaxiukun@yiyc.com>
2016-08-05 13:50:59 +0800

    competition cover  index sources

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @competition = Competition.where.not(status: 0).limit(2)
+    @activity = Activity.where(status: 1).last
   end
 
   def error_404
