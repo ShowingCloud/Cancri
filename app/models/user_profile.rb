d2bcbaef4b1593eddc852452ed5553e2ec3159ff
huaxiukun <huaxiukun@yiyc.com>
2016-12-09 20:21:11 +0800

    remove identify_code

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -23,8 +23,8 @@ class UserProfile < ApplicationRecord
       errors[:username] << '只能包含2-10位中文或英文'
     end
 
-    if (grade.present? && ([grade.to_i] & [10, 11, 12]).present?) && (/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/.match(identity_card) == nil)
-      errors[:grade] << '高中生请正确填写18位身份证号'
-    end
+    # if (grade.present? && ([grade.to_i] & [10, 11, 12]).present?) && (/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/.match(identity_card) == nil)
+    #   errors[:grade] << '高中生请正确填写18位身份证号'
+    # end
   end
 end
