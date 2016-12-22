f2c536c253e41eebd7171a1f7dc485fe7f3e54f5
huaxiukun <huaxiukun@yiyc.com>
2016-12-22 17:23:05 +0800

    users info

---
+++ app/controllers/api/v1/schools_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class SchoolsController < Api::V1::ApplicationController
+      before_action :authenticate_admin
+
+      # 获取特定区县学校列表
+      # GET /api/v1/schools/get_by_district
+
+      def get_by_district
+        requires! :district_id
+        schools = School.where(status: 1, district_id: params[:district_id]).select(:id, :name).order(:name)
+        render json: schools
+      end
+
+    end
+  end
+end
