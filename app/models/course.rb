627ffa3300ec6c36ea5325bb88c0783b9f8e94b3
huaxiukun <huaxiukun@yiyc.com>
2017-02-17 12:41:28 +0800

    fix integer

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,7 +8,7 @@ class Course < ApplicationRecord
   has_many :course_opus, through: :course_user_ships
   validates :name, presence: true, uniqueness: true
   validates :target, presence: true, format: {with: /\A[\u4e00-\u9fa5]+\Z/i, message: '请用汉字描述'}
-  validates :num, presence: true, format: {with: /\A[1-9]\d+\Z/i, message: '只能为整数'}
+  validates :num, presence: true, numericality: {:greater_than => 0}
   validates :run_time, presence: true
   validates :run_address, presence: true
   validates :status, presence: true
