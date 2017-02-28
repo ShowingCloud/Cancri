60c66c7722a8b67fa85ab2d5dae6c09cfcaf3730
WHT <wht@WHTdeMacBook-Pro.local>
2017-02-28 17:18:01 +0800

    improve download comp voucher

--- app/assets/javascripts/comp_voucher.js
+++ app/assets/javascripts/comp_voucher.js
@@ -1,52 +1,58 @@
 /**
  * Created by huaxiukun on 16/8/15.
  */
-function download_comp_voucher(user_info) {
-
-    var user = JSON.parse(user_info);
-    var age = getAge(user.age);
-    var user_gender = getGender(user.gender);
-    var group = getGroup(user.group);
-    var $qrcodeCanvas = $('#qrcodeCanvas');
-
-    $qrcodeCanvas.qrcode({
-        width: 260,
-        height: 260,
-        text: utf16to8("姓名:" + user.username + ";\n性别:" + user_gender + ";\n学校:" + user.school_name + ";\n项目:" + user.event_name + ";\n组别:" + group + ";\n队伍:" + user.identifier + "")
-    });
+function download_comp_voucher(e,user_info) {
+    if (!user_info) {
+        return false;
+    }
     var canvas = document.getElementById("myCanvas");
-    if (canvas.getContext) {
+    if (canvas.getContext && canvas.getContext('2d')) {
+        var user = user_info;
+        user.age = getAge(user.age);
+        user.gender = getGender(user.gender);
+        user.group = getGroup(user.group);
+        var $qrcodeCanvas = $('#qrcodeCanvas');
+        var txt_position = {
+          username: [300, 165],
+          gender:[300, 250],
+          school_name:[300, 338],
+          event_name:[300, 1198],
+          identifier:[300, 1308],
+          teacher:[300, 1408],
+          teacher_mobile:[300, 1505]
+        };
+
+        $qrcodeCanvas.qrcode({
+            width: 260,
+            height: 260,
+            text: utf16to8("姓名:" + user.username + ";\n性别:" + user.gender + ";\n学校:" + user.school_name + ";\n项目:" + user.event_name + ";\n组别:" + user.group + ";\n队伍:" + user.identifier + "")
+        });
+
         var ctx = canvas.getContext("2d");
+        var back_image = document.getElementById("use-voucher");
+        ctx.drawImage(back_image, 0, 0);
         ctx.font = "Normal 30px Arial";
         ctx.textAlign = "left";
         ctx.fillStyle = "black";
+        for(var pro in txt_position){
+          ctx.fillText((user_info[pro] || ""), txt_position[pro][0], txt_position[pro][1]);
+        }
 
-        var back_image = document.getElementById("use-voucher");
-        ctx.drawImage(back_image, 0, 0);
-        ctx.fillText(user["username"], 300, 165);
-        ctx.fillText(age, 880, 165);
-        ctx.fillText(user_gender, 300, 250);
-        ctx.fillText(user["school_name"], 300, 338);
-        ctx.fillText(user["event_name"], 300, 1198);
-        ctx.fillText(user["identifier"], 300, 1308);
-        ctx.fillText(user["teacher"], 300, 1408);
-        ctx.fillText(user["teacher_mobile"], 300, 1505);
         ctx.font = "Normal 50px Arial";
         ctx.fillStyle = "white";
-        ctx.fillText(user["comp_name"], 380, 765);
+        ctx.fillText(user.comp_name, 380, 765);
 
         var qrcode_canvas = $qrcodeCanvas.find("canvas")[0];
         ctx.drawImage(qrcode_canvas, 785, 1266);
-        var all_img = new Image();
-        all_img.src = canvas.toDataURL("image/jpeg");
 
+        var doc = new jsPDF();
+        doc.addImage(canvas.toDataURL("image/jpeg"), 'JPEG', 0, 0, 210, 298);
+        doc.setTextColor(255, 255, 255);
+        e.preventDefault();
+        doc.save(user.identifier + '_' + user.username + '_' + user.event_name + ".pdf");
     }
-    var doc = new jsPDF();
-    var js_pdf_data = all_img.src;
-    doc.addImage(js_pdf_data, 'JPEG', 0, 0, 210, 298);
-    doc.setTextColor(255, 255, 255);
-    doc.save(user["identifier"] + '_' + user["username"] + '_' + user["event_name"]);
 }
+
 function utf16to8(str) {
     var out, i, len, c;
     out = "";
