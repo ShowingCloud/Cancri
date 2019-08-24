852b0313c24571d669b6ff67b31ec31554f7821c
www-data @ SCS.IM <www-data@scs.im>
2019-08-24 13:08:14 -0400

    Renewed startup scripts

--- script/production_start_server.sh
+++ script/production_start_server.sh
@@ -3,6 +3,5 @@
 set -x
 
 sidekiqctl stop tmp/pids/sidekiq.pid 60
-thin -p 3240 -e production stop
-thin -d -p 3240 -e production --tag "Robodou Production" start
+pumactl restart
 sidekiq -C config/sidekiq.yml -d -e production --tag "Robodou Production"
